235cd1fb04f1ebf482bf3f058d663af684fb4684
Tomiris Baktybayeva <64012749+tomariza@users.noreply.github.com>
2022-10-07 15:27:55 +0600

    Create ClaimsModel.go

---
+++ models/ClaimsModel.go
@@ -0,0 +1,29 @@
+package models
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+type JwtClaims struct {
+	Username  string `json:"username,omitempty"`
+	Roles     []int  `json:"roles,omitempty"`
+	jwt.StandardClaims
+}
+
+const ip = "192.168.0.107"
+
+func (claims JwtClaims) Valid() error {
+	var now = time.Now().UTC().Unix()
+	if claims.VerifyExpiresAt(now, true) && claims.VerifyIssuer(ip, true) {
+		return nil	
+	}
+	return fmt.Errorf("Token is invalid")
+}
+
+func (claims JwtClaims) VerifyAudience(origin string) bool {
+	return strings.Compare(claims.Audience, origin) == 0
+}
